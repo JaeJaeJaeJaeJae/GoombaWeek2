d9add28b89c8f20cb189bea00a0300f271a3e448
JaeJaeJaeJaeJae <2015085918@ust-ics.mygbiz.com>
2018-08-24 12:52:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/goomba/goombaweek2/GoombappWeek2.java
@@ -0,0 +1,13 @@
+package com.example.goomba.goombaweek2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoombappWeek2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goombapp_week2);
+    }
+}
